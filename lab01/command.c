d172930743047a50ab1205f23f7f25f8846447f3
RenzoSC <renzoc009@gmail.com>
2023-08-27 20:57:12 -0300

    .

--- lab01/command.c
+++ lab01/command.c
@@ -21,7 +21,11 @@ scommand scommand_new(void){
 }
 
 scommand scommand_destroy(scommand self){
+<<<<<<< Updated upstream
     g_list_free (self->cmdargs);
+=======
+    g_list_free_full(self->cmdargs, (GDestroyNotify)*free);
+>>>>>>> Stashed changes
     self->redout = NULL;
     self->redin = NULL;
     free(self);
@@ -34,6 +38,7 @@ bool scommand_is_empty(const scommand self){
 }
 
 void scommand_push_back(scommand self, char * argument){
+<<<<<<< Updated upstream
     self->cmdargs = g_list_prepend(self->cmdargs, argument);
     return self;
 }
@@ -42,4 +47,95 @@ void scommand_pop_front(scommand self){
     Glist * last = g_list_last(self->cmdargs);
     self->cmdargs = g_list_remove_link(self->cmdargs, last);
     return self;
+=======
+    self->cmdargs = g_list_append(self->cmdargs, argument);
+}
+
+void scommand_pop_front(scommand self){
+    free(g_list_first(self->args)->data);
+    self->cmdargs = g_list_delete_link(self->cmdargs, self->cmdargs);
+}
+
+void scommand_set_redir_in(scommand self, char * filename){
+    self->redin = filename;
+}
+
+void scommand_set_redir_out(scommand self, char * filename){
+    self->redout = filename;
+}
+
+unsigned int scommand_length(const scommand self){
+    return g_list_length (self->cmdargs);
+}
+
+char * scommand_front(const scommand self){
+    return g_list_first(self->cmdargs)->data;
+}
+
+char * scommand_get_redir_in(const scommand self){
+    return self->redin;
+}
+char * scommand_get_redir_out(const scommand self){
+    return self->redout;
+}
+
+char * scommand_to_string(const scommand self){
+    /*Lo termino dsp xdddd*/
+    return NULL;
+}
+
+struct pipeline_s{
+    GList *scmd,
+    bool wait
+}
+
+pipeline pipeline_new(void) {
+    struct pipeline_s *pipe = malloc(sizeof(struct pipeline_s));
+    pipe->scmdList = NULL;
+    pipe->wait = true;
+    return pipe;
+}
+
+static void sdestroy_scommand(scommand cmd) { scommand_destroy(cmd); }
+
+pipeline pipeline_destroy(pipeline self){
+    g_list_free_full(self->scmdList, (GDestroyNotify)&sdestroy_scommand);
+    free(self);
+    self = NULL;
+    return self;
+}
+
+void pipeline_push_back(pipeline self, scommand sc){
+    self->scmdList = g_list_append(self->scmdList, sc);
+}
+
+void pipeline_pop_front(pipeline self){
+    scommand_destroy(g_list_first(self->scmdList)->data);
+    self->scmdList = g_list_delete_link(self->scmdList, self->scmdList);
+}
+
+void pipeline_set_wait(pipeline self, const bool w){
+    self->wait = w;
+}
+
+bool pipeline_is_empty(const pipeline self){
+    return self->scmdList == NULL;
+}
+
+unsigned int pipeline_length(const pipeline self){
+    return g_list_length (self->scmdList);
+}
+
+scommand pipeline_front(const pipeline self){
+    return g_list_first(pipe->scmdList)->data;
+}
+
+bool pipeline_get_wait(const pipeline self){
+    return self->wait;
+}
+
+char * pipeline_to_string(const pipeline self){
+    /* Lo termino dsp xddd */
+    return NULL;
+>>>>>>> Stashed changes
 }
--- lab01/parsing.c
+++ lab01/parsing.c
@@ -7,33 +7,104 @@
 
 static scommand parse_scommand(Parser p) {
     /* Devuelve NULL cuando hay un error de parseo */
-    return NULL;
+
+    bool startpipe = false;
+    bool startbackground = false;                        
+
+    parser_skip_blanks(p);                  //Para ver que al comienzo de un comando simple no haya      
+    parser_op_background(p, &startpipe);    //ni "|" ni "&"(si tenemos algo valido | otro valido y estamos parseando
+    parser_op_pipe(p, &startbackground);    //"otro valido" no va a tomar a "|" al principio pues en la funcion        
+    if(startpipe || startbackground) {      //parse_pipeline se analiza esto antes de entrar a parse_scommand).*/
+        printf("error de sintaxis\n");
+        return NULL;
+    }
+
+
+    scommand command = scommand_new();
+    arg_kind_t type;
+    char* arg;
+
+    while(!parser_at_eof(p)){
+
+        /*parser_skip_blanks(p);
+        parser_op_background(p,delbackground);
+        parser_op_pipe(p,delpipe);
+        if (*delbackground || *delpipe) {
+            break;
+        }*/
+
+        arg = parser_next_argument(p,&type);    //¿Que hace la funcion cuando ve un &?
+                                                //¿El & es tomado tambien como argumento normal?
+        /*if (porelmomento == NULL) {
+            printf("se llego al final\n");    // no se que pasa cuando devuelve NULL         
+            break;
+        }*/
+
+        if(type == ARG_INPUT) {
+            scommand_set_redir_in(command, arg);
+        } else if(type == ARG_OUTPUT) {
+            scommand_set_redir_out(command, arg);
+        } else if(type == ARG_NORMAL) {
+            scommand_push_back(command, arg);       
+        } else {
+            command = scommand_destroy(command);    //Si encuentra algun simbolo que no sea ninguno de
+            return NULL;                            // los tres tipos
+        }
+        parser_skip_blanks(p);
+    }
+
+    return command;
 }
+
+
 
 pipeline parse_pipeline(Parser p) {
     pipeline result = pipeline_new();
     scommand cmd = NULL;
-    bool error = false, another_pipe=true;
+    bool error = false,                         
+    another_pipe=true;
 
-    cmd = parse_scommand(p);
-    error = (cmd==NULL); /* Comando inválido al empezar */
+    cmd = parse_scommand(p); 
+    error = (cmd==NULL);        
     while (another_pipe && !error) {
-        /*
-         * COMPLETAR
-         *
-         */
+        pipeline_push_back(result, cmd);
+        parser_op_pipe(p, &another_pipe);
+        if(another_pipe) {
+            cmd = parse_scommand(p); 
+            error = (cmd==NULL);
+        }
     }
+<<<<<<< Updated upstream
     /* Opcionalmente un OP_BACKGROUND al final */
     /*
      *
      * COMPLETAR
      *
      */
+=======
+>>>>>>> Stashed changes
 
-    /* Tolerancia a espacios posteriores */
-    /* Consumir todo lo que hay inclusive el \n */
-    /* Si hubo error, hacemos cleanup */
+    if (error) {
+        /*bool garbage = false;
+        parser_garbage(p, &garbage);
+        char *garbge = parser_last_garbage(p);
+        printf("%s: No se reconoce como comando valido\n", garbge);  ??? */
+        result = pipeline_destroy(result);
+    } else if (!error && !parser_at_eof(p)) {                     /*¿Tiene sentido este else?*/
+        bool op_background = false;
+        parser_op_background(p, &op_background);            /* Opcionalmente un OP_BACKGROUND al final */
+        if (!op_background) {                                // ?????
+            pipeline_set_wait(result, op_background)
+        }    
+    }
+    bool garbage = false;
+    parser_garbage(p, &garbage);                /* Tolerancia a espacios posteriores */  
+    //char *garbge = parser_last_garbage(p);      /* Consumir todo lo que hay inclusive el \n */
+    if (garbage) {
+        result = pipeline_destroy(result);           /* Si hubo error, hacemos cleanup */
+    }
 
-    return NULL; // MODIFICAR
+    return result; // MODIFICAR
 }
+
 
